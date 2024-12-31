fc41e48f8e0153065ecfd6dad519d3c1bc2a03ed
m0hc33n <123310235+m0hc33n@users.noreply.github.com>
2024-12-31 18:39:21 +0100

    update

--- src/main.c
+++ src/main.c
@@ -44,7 +44,7 @@ int main(int ac, char **av)
 	else
 		push_swap(&stack_a, &stack_b);
 
-	//print_stack(stack_a);
+	print_stack(stack_a);
 
 	free_links(&stack_a);
 	free_links(&stack_b); 
--- src/push_swap_utils2.c
+++ src/push_swap_utils2.c
@@ -21,14 +21,15 @@ static int	get_a_min_ordred_index(t_stack *stack_a)
 static size_t	get_a_link_target(t_stack *stack_a, int b_ordred_index_link)
 {
 	int		is_rot;
-	int		target_pos;
+	size_t	target_pos;
 	size_t	prev_target;
 
 	is_rot = 0;
 	prev_target = INT64_MAX;
+	target_pos = INT64_MAX;
 	while (!is_rot)
 	{
-		if (stack_a->ordred_index > b_ordred_index_link 
+		if (stack_a->ordred_index > b_ordred_index_link
 			&& (size_t)stack_a->ordred_index < prev_target)
 		{
 			target_pos = stack_a->index;
@@ -41,13 +42,12 @@ static size_t	get_a_link_target(t_stack *stack_a, int b_ordred_index_link)
 	return (target_pos);
 }
 
-static void	calc_cost(t_stack *stack_a, t_stack *stack_b, int *cost_a, int *cost_b)
+static void	calc_cost(t_stack *stack_a, t_stack *stack_b, int sz_stack_b, int *cost_a, int *cost_b)
 {
-	int		sz_stack_b;
+	//int		sz_stack_b;
 	int		sz_stack_a;
 	size_t	link_target;
 
-	sz_stack_b = stack_b->blink->index + 1;
 	sz_stack_a = stack_a->blink->index + 1;
 	if (stack_b->index > sz_stack_b / 2)
 		*cost_b = stack_b->index - sz_stack_b;
@@ -65,18 +65,17 @@ static void	calc_cost(t_stack *stack_a, t_stack *stack_b, int *cost_a, int *cost
 void	get_target_b_link(t_stack *stack_a, t_stack *stack_b, int *cost_a, int *cost_b)
 {
 	int		is_rot;
-	int		lowest_cost;
-	int		tcost;
+	size_t	lowest_cost;
+	size_t	tcost;
 	int		tmp_cost_a;
 	int		tmp_cost_b;
-	t_stack	*p_stack_b;
 
 	is_rot = 0;
-	lowest_cost = INT32_MAX;
-	p_stack_b = stack_b;
+	lowest_cost = INT64_MAX;
+	int sz_b = stack_b->blink->index + 1;
 	while (!is_rot)
 	{
-		calc_cost(stack_a, stack_b, &tmp_cost_a, &tmp_cost_b);
+		calc_cost(stack_a, stack_b, sz_b, &tmp_cost_a, &tmp_cost_b);
 		tcost = abs(tmp_cost_a) + abs(tmp_cost_b);
 		if (lowest_cost > tcost)
 		{
@@ -84,8 +83,8 @@ void	get_target_b_link(t_stack *stack_a, t_stack *stack_b, int *cost_a, int *cos
 			*cost_b = tmp_cost_b;
 			lowest_cost = tcost;
 		}
-		p_stack_b = p_stack_b->flink;
-		if (p_stack_b->index == 0)
+		stack_b = stack_b->flink;
+		if (stack_b->index == 0)
 			is_rot = 1;
 	}
 }
